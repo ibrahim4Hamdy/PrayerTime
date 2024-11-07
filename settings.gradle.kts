ec41c30992d0430ce944e3e1e56002e98ea001f5
Ibrahim Elfoly <88224966+ibrahim4Hamdy@users.noreply.github.com>
2024-11-07 08:41:25 +0200

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,1 @@
-pluginManagement {
-    repositories {
-        google()
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
-rootProject.name = "PrayerTime"
-include(":app")
 include(":app:PrayerTime")
